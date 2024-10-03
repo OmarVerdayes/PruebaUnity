e9394cd182cbcf4709b73c70310dafba28fc2e49
OmarVerdayes <20213tn042@utez.edu.mx>
2024-10-03 15:36:40 -0600

    sonido

--- Assets/scripts/Coin.cs
+++ Assets/scripts/Coin.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public AudioSource moneda;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+
             Debug.Log("Ganaste una moneda");
+            moneda.PlayOneShot(moneda.clip);
             Destroy(this.gameObject);
         }
     }
